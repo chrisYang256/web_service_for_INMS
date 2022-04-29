aedaaaa89b64281217e30a976219c27615cafe41
chrisYang256 <maxsummer256@gmail.com>
2022-04-29 22:31:57 +0900

    test: github grass

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ if __name__=="__main__":
     app.run(
         host  = os.getenv("WEB_HOST"),
         port  = os.getenv("WEB_PORT"),
-        debug = os.getenv("DEBUG")
+        debug = os.getenv("DEBUG"),
     )
